b990c4f04d1a00d02e5a7ff3c4a73850ee55898f
mehrdad-esmaeeli <mehrdad.esmaeeli75@gmail.com>
2023-10-03 19:10:44 +0330

    one cheanged

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return "milad esmaeeli and mehrdad esmaeeli";
+    return "milad esmaeeli and mehrdad esmaeeli sef";
 });
